437438cf1b51b8db281ad9bd487a0415aad71abd
Jian Hao <vernonht@gmail.com>
2019-05-21 15:58:41 +0800

    init

---
+++ pages/attendance.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex" v-if="detected == ''">
+    <v-quagga :onDetected="logIt" :readerSize="readerSize" :readerTypes="['code_128_reader']"></v-quagga>
+  </div>
+  <div class="flex" v-else>
+      {{ detected }}
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueQuagga from 'vue-quaggajs';
+
+// register component 'v-quagga'
+Vue.use(VueQuagga);
+
+export default {
+  name: 'VueBarcodeTest',
+  data () {
+    return {
+      readerSize: {
+        width: 100,
+        height: 100
+      },
+      detected: ''
+    }
+  },
+  methods: {
+    logIt (data) {
+        this.detected = data.codeResult.code
+        // this.$router.push(`/${data.codeResult.code}`)
+      console.log('detected', data)
+    }
+
+  }
+}
+</script>
